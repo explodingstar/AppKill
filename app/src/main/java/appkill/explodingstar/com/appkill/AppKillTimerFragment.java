684444888e874d07aa8f04f61cb3463efaecd60c
explodingstar <flyingasian101@gmail.com>
2016-02-13 17:55:38 -0500

    first commit

---
+++ app/src/main/java/appkill/explodingstar/com/appkill/AppKillTimerFragment.java
@@ -0,0 +1,22 @@
+package appkill.explodingstar.com.appkill;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AppKillTimerFragment extends Fragment {
+
+    public AppKillTimerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_app_kill_timer, container, false);
+    }
+}
